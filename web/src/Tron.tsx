4e84328e6b710fd4123d02ba18e6123f5baf99b2
David Vázquez Púa <davazp@gmail.com>
2018-07-10 01:23:59 +0200

    Render basic rectangle in the canvas

--- web/src/Tron.tsx
+++ web/src/Tron.tsx
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
+import createTron from "./TronCanvas";
 
-class Tron extends Component {
+interface State {
+  error: boolean;
+}
+
+class Tron extends Component<{}, State> {
   canvas: HTMLCanvasElement | null = null;
+  state = {
+    error: false
+  };
 
   registerCanvas = (element: HTMLCanvasElement) => {
     this.canvas = element;
   };
 
   componentDidMount() {
-    return;
+    if (!this.canvas) return;
+    const ctx = this.canvas.getContext("2d");
+    if (!ctx) {
+      this.setState({ error: true });
+    } else {
+      createTron(ctx);
+    }
   }
 
   render() {
-    return <canvas ref={this.registerCanvas} />;
+    if (this.state.error) {
+      return <p>Could not render canvas.</p>;
+    } else {
+      return <canvas ref={this.registerCanvas} />;
+    }
   }
 }
 
---
+++ web/src/TronCanvas.ts
@@ -0,0 +1,3 @@
+export default function createTron(ctx: CanvasRenderingContext2D) {
+  ctx.fillRect(10, 10, 400, 400);
+}
