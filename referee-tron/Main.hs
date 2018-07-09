557437aa6af980361dfd05ede9876e6d23c32d2d
David Vázquez Púa <davazp@gmail.com>
2018-07-09 21:35:14 +0200

    Referee handles basic command line arguments

--- referee-tron/Main.hs
+++ referee-tron/Main.hs
@@ -1,5 +1,16 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Exit (die)
+
+mainWithArgs :: [String] -> IO ()
+mainWithArgs [url1, url2] = do
+ putStrLn $ "Player 1: " ++ url1
+ putStrLn $ "Player 2: " ++ url2
+
+mainWithArgs _ =
+   die "Usage: aiplay-referee-tron <url1> <url2>"
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = getArgs >>= mainWithArgs
+
