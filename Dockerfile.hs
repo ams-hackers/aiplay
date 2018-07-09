7f9a82a5f109c8ed2bc1cc09800b2dfc0e266819
David Vázquez Púa <davazp@gmail.com>
2018-07-09 23:10:55 +0200

    Create basic Dockerfile and docker-compose.yml

---
+++ Dockerfile.hs
@@ -0,0 +1,17 @@
+FROM haskell:8.4 as build
+
+WORKDIR /usr/src/app/
+
+COPY stack.yaml ./
+RUN stack setup
+
+COPY package.yaml ./
+RUN stack build --only-dependencies
+
+COPY . ./
+RUN stack build --copy-bins --local-bin-path /usr/local/bin
+
+FROM debian:9
+RUN apt-get update && apt-get install libgmp10
+COPY --from=build /usr/local/bin/aiplay-manager /usr/local/bin/
+COPY --from=build /usr/local/bin/aiplay-referee-tron /usr/local/bin/
