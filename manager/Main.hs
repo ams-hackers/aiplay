fd1f9b71d6296fcdc3c7e2a6529810d22b0ba210
David Vázquez Púa <davazp@gmail.com>
2018-07-12 01:01:18 +0200

    Separate multiple hpack packages

--- manager/Main.hs
+++ manager/Main.hs
@@ -2,17 +2,15 @@
 
 module Main where
 
-import System.Directory
-import Database.PostgreSQL.Simple
+-- import System.Directory
+-- import Database.PostgreSQL.Simple
 
-
-hello :: IO Int
-hello = do
-  conn <- connectPostgreSQL "postgres://aiplay:aiplay@localhost/aiplay"
-  [Only i] <- query_ conn "select 2 + 2"
-  return i
+-- hello :: IO Int
+-- hello = do
+--   conn <- connectPostgreSQL "postgres://aiplay:aiplay@localhost/aiplay"
+--   [Only i] <- query_ conn "select 2 + 5"
+--   return i
 
 main :: IO ()
 main = do
-  n <- hello
-  putStrLn (show n)
+  putStrLn "hello world"
