0731396edcd798e438ec0f84d4a3e66d62561057
David Vázquez Púa <davazp@gmail.com>
2018-07-12 01:22:43 +0200

    Define internal libraries for the multiple components

--- manager/Main.hs
+++ manager/Main.hs
@@ -2,17 +2,5 @@
 
 module Main where
 
-import System.Directory
-import Database.PostgreSQL.Simple
-
-
-hello :: IO Int
-hello = do
-  conn <- connectPostgreSQL "postgres://aiplay:aiplay@localhost/aiplay"
-  [Only i] <- query_ conn "select 2 + 2"
-  return i
-
 main :: IO ()
-main = do
-  n <- hello
-  putStrLn (show n)
+main = putStrLn "Hello manager!"
