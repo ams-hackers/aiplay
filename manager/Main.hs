a184f76640b790a0465ffbab515b54c5f1d3235c
David Vázquez Púa <davazp@gmail.com>
2018-07-12 01:34:53 +0200

    Revert "Define internal libraries for the multiple components"
    This reverts commit 0731396edcd798e438ec0f84d4a3e66d62561057.

--- manager/Main.hs
+++ manager/Main.hs
@@ -2,5 +2,17 @@
 
 module Main where
 
+import System.Directory
+import Database.PostgreSQL.Simple
+
+
+hello :: IO Int
+hello = do
+  conn <- connectPostgreSQL "postgres://aiplay:aiplay@localhost/aiplay"
+  [Only i] <- query_ conn "select 2 + 2"
+  return i
+
 main :: IO ()
-main = putStrLn "Hello manager!"
+main = do
+  n <- hello
+  putStrLn (show n)
